b1293181ee83b62f195ca7639db07af9e87f45db
Mark Drozd <markdrozd.1@gmail.com>
2021-10-19 13:27:02 -0500

    ProjectMMD compiles

--- reproduce_BufferStockTheory.sh
+++ reproduce_BufferStockTheory.sh
@@ -7,7 +7,7 @@ scriptDir="$(dirname "$0")"
 
 echo '' ; echo 'Reproduce text of paper:' ; echo ''
 
-texname=ProjectMMD
+texname=BufferStockTheory
 output_directory='LaTeX'
 
 # Make figures that get made by executing a latex file
